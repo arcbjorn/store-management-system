eb74d7fd87b17868a46d2bbc2bcf2f4215c20b3e
Oleg Luganskiy <arky.happ@gmail.com>
2021-11-21 19:04:38 +0300

    feat (serializer): add proto to JSON converter

--- serializer/file.go
+++ serializer/file.go
@@ -7,6 +7,22 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+func WriteProtobufToJsonFile(message proto.Message, filename string) error {
+	data, err := ProtobufToJsonBytes(message)
+
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to JSON: %w", err)
+	}
+
+	err = ioutil.WriteFile(filename, data, 0644)
+
+	if err != nil {
+		return fmt.Errorf("cannot write JSON data to file: %w", err)
+	}
+
+	return nil
+}
+
 // Convert proto buffer to binary file
 func WriteProtobufToBinaryFile(message proto.Message, filename string) error {
 	data, err := proto.Marshal(message)
