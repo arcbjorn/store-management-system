95ab40ccf5c1190ab59158f5525c7ee60be843ce
Oleg Luganskiy <arky.happ@gmail.com>
2021-11-21 17:42:50 +0300

    test (serializer): proto from binary reader

--- serializer/file_test.go
+++ serializer/file_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 
+	"github.com/arcbjorn/store-management-system/pb/laptop"
 	"github.com/arcbjorn/store-management-system/sample"
 	"github.com/arcbjorn/store-management-system/serializer"
 )
@@ -17,4 +19,9 @@ func TestFileSerializer(t *testing.T) {
 	laptop1 := sample.NewLaptop()
 	err := serializer.WriteProtobufToBinaryFile(laptop1, binaryFile)
 	require.NoError(t, err)
+
+	laptop2 := &laptop.Laptop{}
+	err = serializer.ReadProtobufToBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err)
+	require.True(t, proto.Equal(laptop1, laptop2))
 }
