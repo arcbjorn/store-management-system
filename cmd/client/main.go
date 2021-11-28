860a9a54e1e3d0a3ab215544152e87d87b1f8cc5
arcbjorn <arky.happ@gmail.com>
2021-11-28 19:15:56 +0300

    feat (cmd): add client

---
+++ cmd/client/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/arcbjorn/store-management-system/pb/laptop"
+	"github.com/arcbjorn/store-management-system/sample"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func main() {
+	serverAddress := flag.String("address", "", "the server address")
+	flag.Parse()
+	log.Printf("dial sever %s", *serverAddress)
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+
+	laptopClient := laptop.NewLaptopServiceClient(conn)
+
+	newLaptop := sample.NewLaptop()
+	req := &laptop.CreateLaptopRequest{
+		Laptop: newLaptop,
+	}
+
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Printf("laptop already exists")
+		} else {
+			log.Fatal("cannot create laptop: ", err)
+		}
+		return
+	}
+
+	log.Printf("created laptop with id: %s", res.Id)
+}
